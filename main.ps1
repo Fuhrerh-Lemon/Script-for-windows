dfa05f940455f86e9587914ac5e16698250ee5bd
(∪.∪ )...zzz...zzz <58157906+Fuhrerh-Lemon@users.noreply.github.com>
2021-03-24 01:22:46 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -12,12 +12,16 @@ param ( # Parametros del script
    [string]$App,
    [string]$Firewall,
    [string]$Cleaner,
-   [string]$Activate
+   [string]$Activate,
+   [string]$Cortana
 
 )
 . .\src\activacion.ps1
 . .\src\servicios.ps1
 . .\src\aplicaciones.ps1
+. .\src\cortana.ps1
+. .\src\firewall.ps1
+. .\src\cleaner.ps1
 function archivos_ocultos {
    # Mostrar archivos ocultos
    reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Hidden" /t REG_DWORD /d 1 /f
@@ -28,10 +32,29 @@ function archivos_ocultos {
 }
 
 if ($Valor.Equals("S")){
-   # Dirección del txt
+   # path del txt
    $SvcTxtPath = ".\text\activacion.txt"
+
    #activar_windows $Activate $SvcTxtPath
-   activar_aplicaciones $App $SvcTxtPath
+   #desinstalar_aplicaciones $App $SvcTxtPath
+   #activar_servicios $Service $SvcTxtPath
+   #cortana_app $Cortana
+   cleaner_app $Cleaner
    #archivos_ocultos
 }
-else{ "Opcion de ayuda: -Help" }
+else{ Write-Warning "
+ ____               _         _    _____           __        __ _             _                      
+/ ___|   ___  _ __ (_) _ __  | |_ |  ___|___   _ __\ \      / /(_) _ __    __| |  ___ __      __ ___ 
+\___ \  / __|| '__|| || '_ \ | __|| |_  / _ \ | '__|\ \ /\ / / | || '_ \  / _` | / _ \\ \ /\ / // __|
+ ___) || (__ | |   | || |_) || |_ |  _|| (_) || |    \ V  V /  | || | | || (_| || (_) |\ V  V / \__ \
+|____/  \___||_|   |_|| .__/  \__||_|   \___/ |_|     \_/\_/   |_||_| |_| \__,_| \___/  \_/\_/  |___/
+                      |_|
+                                                                     Autores: FuhrerhLemon, AlexOchoa
+Opciones del script: 
+                     -Valor (S)                : Confirmación requerida, para inciar el script
+                     -Service (start o stop)
+                     -App (remove)
+                     -Activate (on)
+                     -Cleaner (S)
+                     -Firewall
+                     -Cortana (off o remove)" }
